d3a5e69e56650553fadadfa4c4071a162468a1a1
Sasha Kosyakova <116301676+StepStudent@users.noreply.github.com>
2022-11-19 14:47:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import inspect
+import colorama
+
+print(inspect.ismodule (colorama))
+print(inspect.isclass(colorama))
+print(inspect.isfunction(colorama))
+help(colorama)
+print("https://pypi.org/project/colorama/")
